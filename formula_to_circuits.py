e8e991be9a6ebc4dcf008347fc219c008776ea7f
Alex Goeßmann <alex.goessmann@web.de>
2025-10-07 13:35:55 +0200

    Added Wolfram number based treatment of generic connectives in mod2bas+

--- formula_to_circuits.py
+++ formula_to_circuits.py
@@ -1,5 +1,7 @@
 import qc_from_bpCP as qcfb
 
+import numpy as np
+
 
 def formula_to_circuits(qc, qubitDict, formula):
     """
@@ -32,27 +34,63 @@ def get_formula_string(formula):
         return "(" + formula[0] + "_" + "_".join(get_formula_string(subf) for subf in formula[1:]) + ")"
 
 
+aliases = {
+    "xor": "neq",
+    "lpas": "pas0",
+    "id": "pas0",
+    "not": "npas0"
+}
+
+
 def get_bpCP(connectiveKey, inColors):
     """
     Generate the basis plus connective decompositions for a given connective and input colors.
-    :param connectiveKey: Same connective strings as in tnreason.application
+    :param connectiveKey: Same connective strings as in tnreason.representation.basisPlus_calculus
     :param inColors: List of input colors
     :return: Mod2-basis+ CP decomposition by a list of slices
     """
+    if connectiveKey in aliases:
+        connectiveKey = aliases[connectiveKey]
+
     if connectiveKey == "and":
         return [{c: 1 for c in inColors}]
     elif connectiveKey == "or":
         return [{}, {c: 0 for c in inColors}]
-    elif connectiveKey == "not":
-        return [{inColors[0]: 0}]
-    else:
-        raise ValueError("Unsupported connective: " + connectiveKey)
+    elif connectiveKey == "eq":
+        return [{inColor: 1 for inColor in inColors}, {inColor: 0 for inColor in inColors}]
+    elif connectiveKey == "imp":
+        return [{}, {**{inColor: 0 for inColor in inColors[:-1]}, inColors[-1]: 0}]
+    elif connectiveKey.startswith("pas"):
+        pos = int(connectiveKey[3:])
+        assert pos < len(inColors)
+        return [{inColors[pos]: 0}]
+    elif connectiveKey.startswith("n"):
+        return [{}] + get_bpCP(connectiveKey[1:], inColors)
+
+    ## Then understood as a Wolfram number
+    try:
+        int(connectiveKey)
+    except:
+        raise ValueError("Function {} is not a Wolfram number".format(connectiveKey))
+
+    binDigits = bin(int(connectiveKey))[2:]  # Since have a prefix 0b for binary variables
+    order = len(inColors)
+    if len(binDigits) != 2 ** order:
+        binDigits = "0" * (2 ** order - len(binDigits)) + binDigits  # Fill length of digits to 2 ** order
+
+    return [{inColor: indices[i] for i, inColor in enumerate(inColors)} for indices in
+            np.ndindex(*[2 for _ in range(order)]) if
+            int(binDigits[2 ** order - 1 - int("".join(map(str, indices)), 2)]) == 1]
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     qc, qubitDict = qcfb.initialize_circuit(["a", "b", "c"])
     formula_to_circuits(qc, qubitDict, ["or", ["not", "b"], "c"])
+
+    qc, qubitDict = qcfb.initialize_circuit(["a", "b", "c"])
+    formula_to_circuits(qc, qubitDict, ["01100001", "a",  "b", "c"])
 
     qc.draw("mpl")
     plt.show()
